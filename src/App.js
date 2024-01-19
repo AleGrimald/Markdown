17a0a48370cc7eedb2845c3e1229a0bb3de176d1
Ale Grimaldi <grimaldialejandro5@gmail.com>
2024-01-19 14:29:50 -0300

    Proyecto 2 de FreeCodeCamp, Markdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {marked} from 'marked';
 
 function App() {
+  const [texto, setTexto]= useState(`
+  # Markdown FCC
+  ## Dev: Grimaldi Oscar Alejandro
+  [Github Repo](https://github.com/AleGrimald?tab=repositories)
+
+  \`
+  const manejoTexto = (e)=>{
+    setTexto(e.target.value);
+  };
+  \`
+
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+  
+  - 500cc de Aceite
+  - 2Kg de Papa
+  - 400g de Queso Cremoso
+
+  > blockquote
+
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+  **Fin del Proyecto N° 2**
+  `);
+
+  marked.setOptions({
+    breaks: true,
+  });
+  
+  const manejoTexto = (e)=>{
+    setTexto(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" cols="30" rows="10" onChange={manejoTexto} value={texto}/>
+      <div id="preview" dangerouslySetInnerHTML={{__html:marked.parse(texto)}}></div>
     </div>
   );
 }
